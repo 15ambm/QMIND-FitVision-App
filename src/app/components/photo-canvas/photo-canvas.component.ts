e46503ff0b41cc6e48e5315016638befc10bb2f2
justinpaoli <justinnpaoli@gmail.com>
2020-02-09 13:32:28 -0500

    fixed flickering of drawn skeleton

--- src/app/components/photo-canvas/photo-canvas.component.ts
+++ src/app/components/photo-canvas/photo-canvas.component.ts
@@ -9,10 +9,11 @@ import * as posenet from '@tensorflow-models/posenet';
 })
 export class PhotoCanvasComponent implements OnInit {
   @ViewChild('photoCanvas', null) photoCanvas: ElementRef<HTMLCanvasElement>;
-  private ctx: CanvasRenderingContext2D;
+  @ViewChild('displayCanvas', null) displayCanvas: ElementRef<HTMLCanvasElement>;
+  private photoCtx: CanvasRenderingContext2D;
+  private displayCtx: CanvasRenderingContext2D;
   private net: posenet.PoseNet;
   pose: posenet.Pose;
-  poseStr: string;
 
   constructor(private cameraPreview: CameraPreview) {
     this.loadPoseNet();
@@ -35,36 +36,41 @@ export class PhotoCanvasComponent implements OnInit {
 
     this.photoCanvas.nativeElement.width = window.screen.width;
     this.photoCanvas.nativeElement.height = window.screen.height;
-    this.ctx = this.photoCanvas.nativeElement.getContext('2d');
+    this.photoCtx = this.photoCanvas.nativeElement.getContext('2d');
+
+    this.displayCanvas.nativeElement.width = window.screen.width;
+    this.displayCanvas.nativeElement.height = window.screen.height;
+    this.displayCtx = this.displayCanvas.nativeElement.getContext('2d');
 
     const image = new Image();
     setInterval(async () => {
       const src = await this.cameraPreview.takeSnapshot();
       console.log(src);
-      image.src = `data:image/jpeg;base64,${src}`;
-    }, 1000 / 100);
+      image.src = `data:image/png;base64,${src}`;
+    }, 1000 / 60);
 
     image.onload = async () => {
-      this.ctx.clearRect(0, 0, this.photoCanvas.nativeElement.width, this.photoCanvas.nativeElement.height);
-      this.ctx.drawImage(image, 0, 0, this.photoCanvas.nativeElement.width, this.photoCanvas.nativeElement.height);
+      this.photoCtx.clearRect(0, 0, this.photoCanvas.nativeElement.width, this.photoCanvas.nativeElement.height);
+      this.photoCtx.drawImage(image, 0, 0, this.photoCanvas.nativeElement.width, this.photoCanvas.nativeElement.height);
       this.pose = await this.net.estimateSinglePose(this.photoCanvas.nativeElement);
       const connectedJoints = posenet.getAdjacentKeyPoints(this.pose.keypoints, 0.5);
-      this.poseStr = JSON.stringify(this.pose);
 
-      this.ctx.strokeStyle = 'blue';
-      this.ctx.lineWidth = 2;
-      this.ctx.fillStyle = 'blue';
+      this.displayCtx.clearRect(0, 0, this.displayCanvas.nativeElement.width, this.displayCanvas.nativeElement.height);
+      this.displayCtx.drawImage(image, 0, 0, this.displayCanvas.nativeElement.width, this.displayCanvas.nativeElement.height);
+      this.displayCtx.strokeStyle = 'blue';
+      this.displayCtx.lineWidth = 2;
+      this.displayCtx.fillStyle = 'blue';
       this.pose.keypoints.forEach(point => {
         if (point.score) {
-          this.ctx.fillRect(point.position.x, point.position.y, 5, 5);
+          this.displayCtx.fillRect(point.position.x, point.position.y, 5, 5);
         }
       });
       connectedJoints.forEach(points => {
-        this.ctx.beginPath();
-        this.ctx.moveTo(points[0].position.x, points[0].position.y);
-        this.ctx.lineTo(points[1].position.x, points[1].position.y);
-        this.ctx.closePath();
-        this.ctx.stroke();
+        this.displayCtx.beginPath();
+        this.displayCtx.moveTo(points[0].position.x, points[0].position.y);
+        this.displayCtx.lineTo(points[1].position.x, points[1].position.y);
+        this.displayCtx.closePath();
+        this.displayCtx.stroke();
       });
     };
   }
